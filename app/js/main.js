580debc450b4626857e575ae531421f257c851e7
aaksi <yyff91@mail.ru>
2021-01-26 13:14:32 +0300

    added page services

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,35 @@
 
 $(function(){
+    $('#fullpage').fullpage({
+        //options here
+        autoScrolling:true,
+        scrollHorizontally: true,
+        sectionSelector: '.full-page',
+        scrollOverflow: true,
+	    menu: '#menu',
+        anchors: ['top', 'info', 'portfolio', 'services',],
+    });
+
+    $('.services__item-title').on("click", function(){
+        const parent = $(this).parent();
+
+            if(parent.hasClass('services__item--active')){
+                parent.removeClass('services__item--active');
+            }else{
+                $('.services__item').removeClass('services__item--active')
+                parent.addClass('services__item--active')
+            }
+     
+    });
+
    
 
 });
+
+
+
+// $('.services__item-list').toggleClass('services__item-list--active')
+// $(".services__item-list--active").css({
+//     "opacity":"0",
+//     "display":"block",
+// }).show().animate({opacity:1})
